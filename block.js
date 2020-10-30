8b6887451b8301e5b7b456d80df174ae96e9ac97
Jacob Stovall <35011687+xxv37062@users.noreply.github.com>
2020-10-29 23:05:52 -0400

    Update block.js
    Reverted to previous non-functioning version.

--- block.js
+++ block.js
@@ -1,18 +1,15 @@
-const origFetch = window.fetch;
-window.fetch = (url, init, ...args) => {
-	if (typeof url === "string") {
-		if (url.includes("/access_token")) {
-			url = url.replace("player_type=site", "player_type=dashboard");
-		} else if (
-			url.includes("/gql") &&
-			init &&
-			typeof init.body === "string" &&
-			init.body.includes("PlaybackAccessToken")
-		) {
-			const newBody = JSON.parse(init.body);
-			newBody.variables.playerType = "dashboard";
-			init.body = JSON.stringify(newBody);
+# https://github.com/uBlockOrigin/uAssets/pull/3517
+twitch-videoad.js application/javascript
+(function() {
+	if ( /(^|\.)twitch\.tv$/.test(document.location.hostname) === false ) { return; }
+	var realFetch = window.fetch;
+	window.fetch = function(input, init) {
+		if ( arguments.length >= 2 && typeof input === 'string' && input.includes('/access_token') ) {
+			var url = new URL(arguments[0]);
+                        url.searchParams.forEach(function(value, key) {
+                            url.searchParams.delete(key);
+                        });
+			url.searchParams.set("player_type", "frontpage");
+			arguments[0] = url.href;
 		}
-	}
-	return origFetch(url, init, ...args);
-};
+		return realFetch.apply(this, arguments);
